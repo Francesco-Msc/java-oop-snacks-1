be9fa9488742ce732068cfcf3d8567855f8d4382
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-22 19:56:14 +0100

    added Appello class with main method to test RegistroStudenti functionality

---
+++ src/org/lessons/java/snack3/Appello.java
@@ -0,0 +1,9 @@
+package org.lessons.java.snack3;
+
+public class Appello {
+    public static void main(String[] args) {
+        RegistroStudenti appello = new RegistroStudenti();
+
+        
+    }
+}

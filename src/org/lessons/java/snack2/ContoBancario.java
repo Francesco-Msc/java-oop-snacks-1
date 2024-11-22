c9d7415c5375af37f2902b8b28389d0517e96ea0
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-22 19:09:44 +0100

    added instance variables and created constructor and initialized saldo to 0

--- src/org/lessons/java/snack2/ContoBancario.java
+++ src/org/lessons/java/snack2/ContoBancario.java
@@ -1,5 +1,11 @@
 package org.lessons.java.snack2;
 
 public class ContoBancario {
-    
+    int numeroConto;
+    private double saldo;
+
+    public ContoBancario(int numeroConto){
+        this.numeroConto = numeroConto;
+        this.saldo = 0.0;
+    }
 }

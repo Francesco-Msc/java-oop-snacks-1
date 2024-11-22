cd0f0ee35019e7e7adf19e73fa7fca60d295d640
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-22 18:56:52 +0100

    Added instance variables and created a constructor to initialize them

--- src/org/lessons/java/snack1/Studente.java
+++ src/org/lessons/java/snack1/Studente.java
@@ -1,5 +1,13 @@
 package org.lessons.java.snack1;
 
 public class Studente {
+    private String nome;
+    private String cognome;
+    private int età;
 
+    public Studente(String nome, String cognome, int età){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
 }
